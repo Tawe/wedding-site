8cb93d1a8f98c92971dfc8b4722b54b51cf634b1
John Munn <jmunn@getchalk.com>
2018-08-26 18:20:59 -0300

    Adding # to title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: '2BecomeMunn',
+    title: '#2BecomeMunn',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: '2BecomeMunn',
-        short_name: '2BecomeMunn',
+        name: '#2BecomeMunn',
+        short_name: '#2BecomeMunn',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
