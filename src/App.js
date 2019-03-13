72b2e242c7362cad136ffc51860ed6b41f2aeb3b
John Munn <jmunn@getchalk.com>
2019-03-13 01:10:41 -0300

    Adding map and font stylings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from 'react'
 import { Reset } from 'styled-reset'
+import { css } from 'styled-components'
+import { injectGlobal } from 'styled-components'
+
 import Home from './components/pages/home'
+
+injectGlobal`
+  @font-face {
+    font-family: brandonMed;
+    src: url('../public/fonts/Brandon_med.otf');
+  }
+
+  @font-face {
+    font-family: brandonReg;
+    src: url('../public/fonts/Brandon_reg.otf');
+  }
+
+  @font-face {
+    font-family: brandonBold;
+    src: url('../public/fonts/Brandon_bld_it.otf');
+  }
+
+  @font-face {
+    font-family: minion;
+    src: url('../public/fonts/MinionPro-Bold.otf');
+  }
+`
 
 function App() {
   return (
--- src/components/molecules/detailsblock/index.js
+++ src/components/molecules/detailsblock/index.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import { Flex } from '@rebass/grid'
+import styled from 'styled-components'
 
 const DetailsBlock = ({ title, bold, info }) => (
   <Flex flexDirection="column" alignItems="center">
-    <span>{title}</span>
-    <span>{bold}</span>
-    <span>{info}</span>
+    <TitleStyles>{title}</TitleStyles>
+    <BoldStyles>{bold}</BoldStyles>
+    <InfoStyles>{info}</InfoStyles>
   </Flex>
 )
+
+const TitleStyles = styled('span')`
+  font-family: ${props => props.theme.fonts.secondary};
+  font-weight: ${props => props.theme.fontweights.bold};
+  font-size: ${props => props.theme.fontsizes[4]};
+`
+
+const BoldStyles = styled('span')`
+  font-family: ${props => props.theme.fonts.primary};
+  font-weight: ${props => props.theme.fontweights.bold};
+  font-size: ${props => props.theme.fontsizes[3]};
+  text-transform: uppercase;
+`
+
+const InfoStyles = styled('span')`
+  font-family: ${props => props.theme.fonts.primary};
+  font-weight: ${props => props.theme.fontweights.medium};
+  font-size: ${props => props.theme.fontsizes[3]};
+`
 
 export default DetailsBlock
