031c09a7975f512b639763f23b3bc013a6077a99
John Munn <munn.jon@gmail.com>
2019-05-06 03:16:06 -0300

    Adding direction text

--- src/components/molecules/rsvpform/index.js
+++ src/components/molecules/rsvpform/index.js
@@ -3,14 +3,18 @@ import { Input } from '../../atoms/input'
 import Button from '../../atoms/button'
 import Label from '../../atoms/label'
 import { Box } from '../../atoms/box'
+import theme from '../../../theme'
 
 const RSVPForm = ({ cb }) => (
   <form onSubmit={cb}>
     <Box mb="8px">
       <Label htmlFor="name">YOUR FULL NAME: </Label>
     </Box>
-    <Box mb="21px" height="51px">
+    <Box mb="30px" height="51px">
       <Input name="name" id="name" />
+      <Box mt="4px" mb="20px" fontSize={theme.fontsizes[1]}>
+        * Make sure to enter your name exactly as it written on your invitation.
+      </Box>
     </Box>
 
     <Box height="51px" width="100%">
