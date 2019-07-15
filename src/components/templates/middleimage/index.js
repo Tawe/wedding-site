cb160548e112dbd90b4e8c793e9fa2adc09fa4ad
John Munn <munn.jon@gmail.com>
2019-07-14 22:04:25 -0300

    Adding visibility sensor to image

--- src/components/templates/middleimage/index.js
+++ src/components/templates/middleimage/index.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BackgroundImage from 'react-background-image'
+import VisibilitySensor from 'react-visibility-sensor'
 
 import { Box } from '../../atoms/box'
 import hdImage from './image2_large.jpg'
 import smallImage from './image2_small.jpg'
 
-const MiddleImages = () => (
-  <Box width="100%" height="564px">
-    <BackgroundImage placeholder={smallImage} src={hdImage} />
-  </Box>
-)
+const MiddleImages = () => {
+  const [state, setState] = useState({
+    visible: false,
+  })
+  const changeVisiblity = isVisible => setState({ visible: isVisible })
+
+  return (
+    <VisibilitySensor
+      onChange={changeVisiblity}
+      partialVisibility
+      active={!state.visible}
+    >
+      {({ isVisible }) => (
+        <>
+          {isVisible ? (
+            <Box width="100%" height="564px">
+              <BackgroundImage placeholder={smallImage} src={hdImage} />
+            </Box>
+          ) : (
+            <Box width="100%" height="564px" />
+          )}
+        </>
+      )}
+    </VisibilitySensor>
+  )
+}
 
 export default MiddleImages
