e6b3d50682d4fcdbcd7f2dd7231a167ab94d2944
John Munn <munn.jon@gmail.com>
2019-07-14 21:15:38 -0300

    testing image loading

--- src/components/templates/hero/index.js
+++ src/components/templates/hero/index.js
@@ -1,61 +1,63 @@
 import React from 'react'
 import Media from 'react-media'
 
+import BackgroundImage from 'react-background-image'
 import theme from '../../../theme'
 import Nav from '../../molecules/nav'
 import MobileNav from '../../molecules/mobilenav'
 import { Box } from '../../atoms/box'
 import Button from '../../atoms/button'
+import hdImage from './hero_large.jpg'
+import smallImage from './hero_small.jpg'
 
 const Hero = ({ menuOpen, setMenuOpen }) => (
-  <Box
-    width="100%"
-    height="100vh"
-    min-height="700px "
-    background="url('./assets/imgs/hero_large.jpg');"
-    backgroundSize="cover"
-    backgroundRepeat="no-repeat"
-    backgroundPosition="center"
-  >
-    <Box
-      position="absolute"
-      background="linear-gradient(359deg, rgba(84, 84, 84, 0) 0%, rgba(0, 0, 0, 1) 100%)"
-      width="100%"
-      height="100%"
-    />
-
-    <Media query="(max-width: 650px)">
-      {matches =>
-        matches ? (
-          <Box
-            top="60px"
-            zIndex="222"
-            position="absolute"
-            width="100%"
-            m="0 auto"
-          >
-            <MobileNav setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
-          </Box>
-        ) : (
-          <Box
-            position="relative"
-            top="78px"
-            zIndex="222"
-            width="100%"
-            maxWidth="650px"
-            m="0 auto"
-          >
-            <Nav />
-          </Box>
-        )
-      }
-    </Media>
-    <HeroTitle {...{ menuOpen }} {...{ setMenuOpen }} />
+  <Box width="100%" height="100vh" min-height="700px ">
+    <BackgroundImage placeholder={smallImage} src={hdImage}>
+      <Box
+        position="absolute"
+        background="linear-gradient(359deg, rgba(84, 84, 84, 0) 0%, rgba(0, 0, 0, 1) 100%)"
+        width="100%"
+        height="100%"
+        zIndex="2"
+      />
+      <Media query="(max-width: 650px)">
+        {matches =>
+          matches ? (
+            <Box
+              top="60px"
+              zIndex="222"
+              position="absolute"
+              width="100%"
+              m="0 auto"
+            >
+              <MobileNav setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
+            </Box>
+          ) : (
+            <Box
+              position="relative"
+              top="78px"
+              zIndex="222"
+              width="100%"
+              maxWidth="650px"
+              m="0 auto"
+            >
+              <Nav />
+            </Box>
+          )
+        }
+      </Media>
+      <HeroTitle {...{ menuOpen }} {...{ setMenuOpen }} />
+    </BackgroundImage>
   </Box>
 )
 
 const HeroTitle = ({ menuOpen, setMenuOpen }) => (
-  <Box pt={['165px', '175px', '275px']} textAlign="center" position="relative">
+  <Box
+    pt={['165px', '175px', '275px']}
+    textAlign="center"
+    position="relative"
+    zIndex="2"
+  >
     <Box
       fontFamily={theme.fonts.tertiary}
       fontSize={theme.fontsizes[3]}
@@ -82,7 +84,7 @@ const HeroTitle = ({ menuOpen, setMenuOpen }) => (
     <Media query="(max-width: 650px)">
       {matches =>
         matches && (
-          <Box mt="60px" width="155px" mx="auto">
+          <Box mt="60px" width="155px" mx="auto" zIndex="2">
             <Button href="#RSVP" type="tertiary">
               RSVP
             </Button>
--- src/components/templates/middleimage/index.js
+++ src/components/templates/middleimage/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
+import BackgroundImage from 'react-background-image'
+
 import { Box } from '../../atoms/box'
+import hdImage from './image2_large.jpg'
+import smallImage from './image2_small.jpg'
 
 const MiddleImages = () => (
-  <Box
-    width="100%"
-    height="564px"
-    background="url('./assets/imgs/image2.jpg')"
-    backgroundSize="cover"
-    backgroundRepeat="no-repeat"
-    backgroundPosition="center"
-  />
+  <Box width="100%" height="564px">
+    <BackgroundImage placeholder={smallImage} src={hdImage} />
+  </Box>
 )
 
 export default MiddleImages
